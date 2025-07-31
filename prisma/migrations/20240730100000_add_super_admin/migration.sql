e7f5853e536ab8d6976e3b2028fc4873d06c31e8
haymi28 <haymidegemu@gmail.com>
2025-07-31 08:31:16 +0000

    Create a super admin user with:
    username: superadmin
    
    role: super_admin

---
+++ prisma/migrations/20240730100000_add_super_admin/migration.sql
@@ -0,0 +1,3 @@
+
+-- AddSuperAdmin
+INSERT INTO "User" ("id", "username", "password", "role", "displayName") VALUES ('clx9q5s9j000008l5c5y1d8g1', 'superadmin', 'Admin123!', 'super_admin', 'ዋና ሃላፊ');
