ba849da4073056eea6f9bbe4ed00a2745328d572
haymi28 <haymidegemu@gmail.com>
2025-07-31 06:16:40 +0000

    Add await before every cookies() call.
    Make sure all functions using co

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,3 @@
-
 'use server'
 
 import { cookies } from 'next/headers'
@@ -14,7 +13,7 @@ export async function signIn(credentials: {username: string, password: string}):
     }
     
     // In a real app, use JWTs or a session library. For this, we'll set simple cookies.
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
     cookieStore.set('auth_token', user.id, { httpOnly: true, path: '/' });
     cookieStore.set('user_role', user.role, { httpOnly: true, path: '/' });
     cookieStore.set('username', user.username, { httpOnly: true, path: '/' });
@@ -24,7 +23,7 @@ export async function signIn(credentials: {username: string, password: string}):
 }
 
 export async function signOut() {
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
     cookieStore.delete('auth_token');
     cookieStore.delete('user_role');
     cookieStore.delete('username');
@@ -32,7 +31,7 @@ export async function signOut() {
 }
 
 export async function getServerSession() {
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
     const token = cookieStore.get('auth_token');
     const role = cookieStore.get('user_role');
     const username = cookieStore.get('username');
