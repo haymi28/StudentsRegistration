3854bd1e7bba312b4af3f84371922a99a59fbbbc
haymi28 <haymidegemu@gmail.com>
2025-09-08 13:51:38 +0000

    Error: A param property was accessed directly with `params.id`. `params`

--- src/app/roles/edit/[id]/page.tsx
+++ src/app/roles/edit/[id]/page.tsx
@@ -14,7 +14,7 @@ import { useState, useEffect } from 'react';
 import { Role } from '@prisma/client';
 
 export default function EditRolePage({ params }: { params: { id: string } }) {
-  const { id } = params;
+  const id = params.id;
   const { t } = useLocale();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
